7b1063e7c1a8456e479d6399479c593f5696bbce
ManavToor <68403400+ManavToor@users.noreply.github.com>
2024-03-01 12:57:08 -0500

    Update TrajectoryPrediction.java

--- src/main/java/com/waterloorocketry/airbrakeplugin/controller/TrajectoryPrediction.java
+++ src/main/java/com/waterloorocketry/airbrakeplugin/controller/TrajectoryPrediction.java
@@ -2,7 +2,7 @@ package com.waterloorocketry.airbrakeplugin.controller;
 
 public class TrajectoryPrediction {
     /**
-     *  @return derivative of velocity: acceleration
+     *  @return acceleration (m/s^2)
      */
     private static double velocity_derivative(double force, double mass) {
         return force/mass;
@@ -10,7 +10,7 @@ public class TrajectoryPrediction {
 
     /**
      * rk4 method to integrate altitude from velocity, and integrate velocity from acceleration (force/mass)
-     * @param h time step to integrate over
+     * @param h time step
      * @param force sum of forces acting on rocket at given time (N)
      * @param mass of rocket (kg)
      * @param integrals, altitude (m) integrals[0] and velocity (m/s) integrals[1]
@@ -37,7 +37,9 @@ public class TrajectoryPrediction {
         return 9.80665 * Math.pow(6371009 / ( 6371009 + altitude), 2);
     }
 
-    /** @return air density from altitude */
+    /** @return air density (kg/m^3)
+     * @param altitude (m)
+     * Same method used for PyAnsys simulations, verified to be correct*/
     private static double air_density(double altitude) {
         // Based on US Standard Atmosphere 1976
 
@@ -59,17 +61,60 @@ public class TrajectoryPrediction {
         return density;
     }
 
-    /** @return  force of drag from drag coefficient, altitude, and velocity */
-    private static double drag_force(double drag_coef, double altitude, double velocity) {
-        double ref_area = 0.01824; // reference area (m^2)
-        double fluid_dens = air_density(altitude); // air density (kg/m^3)
-
-        // Fd = 0.5 * Cd * A * p * v^2
-        return -0.5 * drag_coef * ref_area * fluid_dens * velocity * velocity;
+    /**@return rocket's cross-sectional area from airbrake extension
+     * Does not take into account fins
+     * 0% extension: 28.274 (in^2)
+     * 100% extension: 36.041 (in^2)
+     * */
+    private static double rocket_area(double extension) {
+        return (0.0774 * extension) + 28.274;
     }
 
-    /** @return max apogee from velocity, altitude, and drag coefficient */
-    public static double get_max_altitude(double velocity, double altitude, double drag_coef, double mass) {
+    /**@return drag force value based off Ansys simulations
+     * @param velocity is used to lookup drag value
+     * @param extension MUST BE EITHER 0, 50, 100. USed to determing which function to use and to adjust for rocket area
+     * air_density is adjusted for since all sims were calculated at 1000m
+     * */
+    private static double lookup_drag(int extension, double velocity) {
+        double drag;
+        if (extension == 0) {
+            drag = (0.0035 * velocity * velocity) + (0.1317 * velocity) - 5.0119;
+        } else if (extension == 50) {
+            drag = (0.0045 * velocity * velocity) + (0.1031 * velocity) - 3.8231;
+        } else { // extension == 100
+            drag = (0.006*velocity*velocity) + (0.1038*velocity) - 4.2522;
+        }
+        return drag / rocket_area(extension) / air_density(1000);
+    }
+
+    /**@return drag force acting on rocket
+     * @param extension of air brakes, used for adjusting rocket area and iterpolating Ansys sims
+     * @param velocity used to lookup drag force from Ansys sims
+     * @param altitude used to adjust fluid density since Ansys sims were calculated at 1000m
+     * */
+    private static double interpolate_drag(double extension, double velocity, double altitude) {
+        if (extension == 100 || extension == 50 ||  extension == 0) {
+            int ext = (int)extension;
+            return air_density(altitude) * rocket_area(ext) * lookup_drag(ext, velocity);
+        }
+        else if (extension > 50.0) {
+            double diff = lookup_drag(100, velocity) - lookup_drag(50, velocity);
+            extension = (extension - 50) * 2;
+            return air_density(altitude) * rocket_area(extension) * (diff * extension) + lookup_drag(50, velocity);
+        }
+        else { // extension < 50
+            double diff = lookup_drag(50, velocity) - lookup_drag(0, velocity);
+            extension *= 2;
+            return air_density(altitude) * rocket_area(extension) * (diff * extension) + lookup_drag(0, velocity);
+        }
+    }
+
+    /** @return max apogee
+     * @param velocity vertical velocity (m/s)
+     * @param altitude (m)
+     * @param airbrake_ext % extension of airbrakes 0-100 NOT 0-1
+     * @param mass (kg)*/
+    public static double get_max_altitude(double velocity, double altitude, double airbrake_ext, double mass) {
 
         double h = 0.01; // interval of change for rk4
         double prevAlt = 0.0; // variable to store previous altitude
@@ -78,15 +123,18 @@ public class TrajectoryPrediction {
         while (alt_vel[0] >= prevAlt) {
             // update forces of drag and gravity from new altitude
             double Fg = -gravitational_acceleration(alt_vel[0]) * mass; // force of gravity (N)
-            double Fd = drag_force(drag_coef, alt_vel[0], alt_vel[1]); // force of drag (N)
+            double Fd = -interpolate_drag(airbrake_ext, alt_vel[1], alt_vel[0]); // force of drag (N)
 
             // to check if altitude is decreasing to exit the loop
             prevAlt = alt_vel[0];
 
             // update velocity and altitude
             rk4(h, Fg+Fd, mass, alt_vel);
+
+            System.out.println(alt_vel[0]);
         }
 
         return alt_vel[0];
     }
 }
+
