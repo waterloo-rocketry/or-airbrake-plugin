cbffe9f2ebfeb5b3ce33609025f877002ee58b8c
Patrick Gu <patrick@patrickgu.ca>
2024-03-07 09:35:46 -0500

    Generalize linear interpolator to compute values based on an arbitrary function and context.
    The idea here is that we can compute drag values for each airbrake extension amount based on the context, then use linear interpolation to compute the drag amount for some specific airbrake extension.

--- src/test/java/com/waterloorocketry/airbrakeplugin/test/simulated/SingleVarLinearInterpTest.java
+++ src/test/java/com/waterloorocketry/airbrakeplugin/test/simulated/SingleVarLinearInterpTest.java
@@ -9,26 +9,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SingleVarLinearInterpTest {
     @Test
     void compute() {
-        SingleVarLinearInterp interp = new SingleVarLinearInterp(
+        SingleVarLinearInterp<Void, SingleVarLinearInterp.ConstantValue> interp = new SingleVarLinearInterp<>(
                 new double[] { 0.0, 1.0, 2.0 },
-                new double[] { 0.0, 2.0, 5.0 }
+                new SingleVarLinearInterp.ConstantValue[] { new SingleVarLinearInterp.ConstantValue(0.0), new SingleVarLinearInterp.ConstantValue(2.0), new SingleVarLinearInterp.ConstantValue(5.0) }
         );
-        assertEquals(interp.compute(0.0), 0.0);
-        assertEquals(interp.compute(1.0), 2.0);
-        assertEquals(interp.compute(2.0), 5.0);
-        assertEquals(interp.compute(0.3), 0.6, 0.00001);
-        assertEquals(interp.compute(1.5), 3.5, 0.00001);
-        assertThrows(IndexOutOfBoundsException.class, () -> interp.compute(-1.0));
-        assertThrows(IndexOutOfBoundsException.class, () -> interp.compute(2.01));
-        assertThrows(IllegalArgumentException.class, () -> interp.compute(Double.NaN));
+        assertEquals(interp.compute(null, 0.0), 0.0);
+        assertEquals(interp.compute(null, 1.0), 2.0);
+        assertEquals(interp.compute(null, 2.0), 5.0);
+        assertEquals(interp.compute(null, 0.3), 0.6, 0.00001);
+        assertEquals(interp.compute(null, 1.5), 3.5, 0.00001);
+        assertThrows(IndexOutOfBoundsException.class, () -> interp.compute(null, -1.0));
+        assertThrows(IndexOutOfBoundsException.class, () -> interp.compute(null, 2.01));
+        assertThrows(IllegalArgumentException.class, () -> interp.compute(null, Double.NaN));
     }
 
     @Test
     void constructor() {
-        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp(new double[0], new double[0]));
-        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp(new double[3], new double[4]));
-        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp(new double[] { Double.NaN, 0.0, 0.2 }, new double[] { 0.0, 0.0, 0.0 }));
-        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp(new double[] { -0.5, 0.0, 0.2 }, new double[] { 0.0, Double.NaN, 0.0 }));
-        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp(new double[] { 0.5, 0.0, 0.2 }, new double[] { 0.0, 0.0, 0.0 }));
+        SingleVarLinearInterp.ConstantValue zero = new SingleVarLinearInterp.ConstantValue(0.0);
+        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp<>(new double[0], new SingleVarLinearInterp.ConstantValue[0]));
+        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp<>(new double[3], new SingleVarLinearInterp.ConstantValue[] { zero, zero, zero, zero }));
+        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp<>(new double[] { Double.NaN, 0.0, 0.2 }, new SingleVarLinearInterp.ConstantValue[] { zero, zero, zero }));
+        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp<>(new double[] { -0.5, 0.0, 0.2 }, new SingleVarLinearInterp.ConstantValue[] { zero, null, zero }));
+        assertThrows(IllegalArgumentException.class, () -> new SingleVarLinearInterp<>(new double[] { 0.5, 0.0, 0.2 }, new SingleVarLinearInterp.ConstantValue[] { zero, zero, zero }));
     }
 }
