db77ac7c83dd08a63ed1faf387cc741fa9e6820e
celery <71736183+celery6@users.noreply.github.com>
2024-03-20 13:10:05 -0400

    add return value for rk4 instead of modifying object passed in

--- src/main/java/com/waterloorocketry/airbrakeplugin/controller/TrajectoryPrediction.java
+++ src/main/java/com/waterloorocketry/airbrakeplugin/controller/TrajectoryPrediction.java
@@ -25,9 +25,10 @@ public class TrajectoryPrediction {
      * @param h time step
      * @param force sum of forces acting on rocket at given time (N)
      * @param mass of rocket (kg)
-     * @param integrals, altitude (m) and velocity (m/s
+     * @param integrals, altitude (m) and velocity (m/s)
+     * @return updated altitude and velocity integrals after one rk4 step
      */
-    private static void rk4(double h, double force, double mass, RK4Integrals integrals) {
+    private static RK4Integrals rk4(double h, double force, double mass, RK4Integrals integrals) {
         double ka1 = h * integrals.alt;
         double kv1 = h * velocity_derivative(force, mass);
 
@@ -40,19 +41,26 @@ public class TrajectoryPrediction {
         double ka4 = h * (integrals.alt + h*ka3);
         double kv4 = h * velocity_derivative(force + h*kv3, mass);
 
-        integrals.vel = (integrals.vel + (ka1 + 2*ka2 + 2*ka3 + ka4)/6);
-        integrals.alt = (integrals.alt + (kv1 + 2*kv2 + 2*kv3 + kv4)/6);
+        RK4Integrals updatedIntegrals = new RK4Integrals();
+        updatedIntegrals.vel = (integrals.vel + (ka1 + 2*ka2 + 2*ka3 + ka4)/6);
+        updatedIntegrals.alt = (integrals.alt + (kv1 + 2*kv2 + 2*kv3 + kv4)/6);
+
+        return updatedIntegrals;
     }
 
-    /** @param altitude (m)
-     * @return acceleration due to gravity (m/s^2) */
+    /**
+     * @param altitude (m)
+     * @return acceleration due to gravity (m/s^2)
+     */
     private static double gravitational_acceleration(double altitude) {
         return GRAV_AT_SEA_LVL * Math.pow(EARTH_MEAN_RADIUS / ( EARTH_MEAN_RADIUS + altitude), 2);
     }
 
-    /** @param altitude (m)
+    /**
+     * @param altitude (m)
      * @return air density (kg/m^3)
-     * Same method used for PyAnsys simulations, verified to be correct */
+     * Same method used for PyAnsys simulations, verified to be correct
+     */
     private static double air_density(double altitude) {
         // Based on US Standard Atmosphere 1976
         double temperature, pressure, density;
@@ -73,17 +81,20 @@ public class TrajectoryPrediction {
         return density;
     }
 
-    /**@return rocket's cross-sectional area from airbrake extension
+    /**
      * Does not take into account fins
-     * @param extension extension of the airbrakes (0-1) */
+     * @param extension extension of the airbrakes (0-1)
+     * @return rocket's cross-sectional area from airbrake extension
+     */
     private static double rocket_area(double extension) {
         return (AIRBRAKES_MAX_AREA * extension) + ROCKET_BASE_AREA;
     }
 
-    /**@return drag force value based on curves produced by Ansys at fixed extensions
+    /**
      * @param velocity is used to lookup drag value
      * @param fixed_extension MUST BE EITHER 0, 0.5, or 1. Used to determine which function to use and to adjust for rocket area
-     * */
+     * @return drag force value based on curves produced by Ansys at fixed extensions
+     */
     private static double lookup_drag(double fixed_extension, double velocity) {
         double drag;
 
@@ -165,7 +176,7 @@ public class TrajectoryPrediction {
             prevAlt = states.alt;
 
             // update velocity and altitude
-            rk4(h, Fg+Fd, mass, states);
+            states = rk4(h, Fg+Fd, mass, states);
 
             // System.out.println("pred alt: " + states.alt + "m");
         }
