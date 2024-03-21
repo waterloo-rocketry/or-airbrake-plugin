454ed3851b3891ff118505454a22a658bae696a9
celery <71736183+celery6@users.noreply.github.com>
2024-03-20 21:33:37 -0400

    add doc comments

--- src/main/java/com/waterloorocketry/airbrakeplugin/controller/TrajectoryPrediction.java
+++ src/main/java/com/waterloorocketry/airbrakeplugin/controller/TrajectoryPrediction.java
@@ -6,6 +6,9 @@ public class TrajectoryPrediction {
     private static final double AIRBRAKES_MAX_AREA = 0.004993538; //m^2
     private static final double ROCKET_BASE_AREA = 0.0182412538; //m^2
     private static final double SIM_ALTITUDE = 1000; //All drag sims conducted at 1000m above sea level
+    /**
+     * Floating point inaccuracy tolerance
+     */
     private static final double TOL = 0.00001;
 
     private static class RK4Integrals {
