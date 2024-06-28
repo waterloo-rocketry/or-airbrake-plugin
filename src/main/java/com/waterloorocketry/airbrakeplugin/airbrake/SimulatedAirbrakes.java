dbd08ee1e3bd9eae107c08185ae892b4727b697f
Patrick Gu <patrick@patrickgu.ca>
2024-06-28 00:31:20 -0400

    Use an airbrakes extension from 0.5 s earlier

--- src/main/java/com/waterloorocketry/airbrakeplugin/airbrake/SimulatedAirbrakes.java
+++ src/main/java/com/waterloorocketry/airbrakeplugin/airbrake/SimulatedAirbrakes.java
@@ -2,6 +2,9 @@ package com.waterloorocketry.airbrakeplugin.airbrake;
 
 import com.waterloorocketry.airbrakeplugin.simulated.SimulatedDragForceInterpolator;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Airbrakes using CFD simulated values
  */
@@ -11,5 +14,17 @@ public class SimulatedAirbrakes implements Airbrakes {
     @Override
     public double calculateDragForce(double extension, double velocity, double altitude) {
         return interp.compute(new SimulatedDragForceInterpolator.Data(extension, velocity, altitude));
+    }
+
+    @Override
+    public double getAppliedAirbrakesExtension(List<Double> timestamps, List<Double> extensions, double currentTimestamp) {
+        int idx = Collections.binarySearch(timestamps, currentTimestamp - 0.5);
+        if (idx >= 0) {
+            return extensions.get(idx);
+        } else if (idx == -1) {
+            return 0.0;
+        } else {
+            return extensions.get(-(idx + 1));
+        }
     }
 }
