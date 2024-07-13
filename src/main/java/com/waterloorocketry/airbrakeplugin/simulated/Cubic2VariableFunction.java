1559c9548f7aae0caa8b7ed8e5cc345b8b34014e
Patrick Gu <patrick@patrickgu.ca>
2024-07-12 23:52:53 -0400

    Polynomials for drag(velocity, altitude)

---
+++ src/main/java/com/waterloorocketry/airbrakeplugin/simulated/Cubic2VariableFunction.java
@@ -0,0 +1,52 @@
+package com.waterloorocketry.airbrakeplugin.simulated;
+
+public class Cubic2VariableFunction implements Interpolator<Cubic2VariableFunction.Coordinate> {
+    private final double meanX;
+    private final double stdX;
+    private final double meanY;
+    private final double stdY;
+    private final double p00;
+    private final double p10;
+    private final double p01;
+    private final double p20;
+    private final double p11;
+    private final double p02;
+    private final double p30;
+    private final double p21;
+    private final double p12;
+    private final double p03;
+
+    public static class Coordinate {
+        public double x;
+        public double y;
+
+        Coordinate(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    Cubic2VariableFunction(double meanX, double stdX, double meanY, double stdY, double p00, double p10, double p01, double p20, double p11, double p02, double p30, double p21, double p12, double p03) {
+        this.meanX = meanX;
+        this.stdX = stdX;
+        this.meanY = meanY;
+        this.stdY = stdY;
+        this.p00 = p00;
+        this.p10 = p10;
+        this.p01 = p01;
+        this.p20 = p20;
+        this.p11 = p11;
+        this.p02 = p02;
+        this.p30 = p30;
+        this.p21 = p21;
+        this.p12 = p12;
+        this.p03 = p03;
+    }
+
+    @Override
+    public double compute(Coordinate coordinate) {
+        double x = (coordinate.x - meanX) / stdX;
+        double y = (coordinate.y - meanY) / stdY;
+        return p00 + p10 * x + p01 * y + p20 * x * x + p11 * x * y + p02 * y * y + p30 * x * x * x + p21 * x * x * y + p12 * x * y * y + p03 * y * y * y;
+    }
+}
